a7c438b3a608090c46d3ba8687c3ea4280ece15b
freeWillee <will.i.lee@outlook.com>
2018-07-20 15:19:17 -0600

    Modified scrapper class -> grabbing name and profile url

--- lib/calgary_gps/scraper.rb
+++ lib/calgary_gps/scraper.rb
@@ -1,15 +1,18 @@
 class CalgaryGps::Scraper
 
-  BASE_URL = "https://www.ratemds.com/best-doctors/ab/calgary/family-gp/?verified=true&accepting_patients=true"
+  BASE_URL = "https://www.ratemds.com/best-doctors/ab/calgary/?verified=true&accepting_patients=true"
 
   def self.scrape_docs
     doc = Nokogiri::HTML(open(BASE_URL))
+    # binding.pry
 
     doc.css(".doctor-profile").each do |doctor|
-      binding.pry
+      name = doctor.css(".search-item-doctor-name a").text.strip
+      profile_url = "https://www.ratemds.com" + doctor.css(".search-item-doctor-name a").attribute('href').value
+      puts name
+      puts profile_url
+      # binding.pry
     end
-
-
   end
 
 end
